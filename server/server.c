0bb8f0c3c1b966f27c372b199e6d6517ecfe6f8a
Matdeatino <120032788+Matdeatino@users.noreply.github.com>
2025-04-25 12:49:14 +0200

    Sistemato segmentation fault + aggiunta nuovi file

--- server/server.c
+++ server/server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include <cjson/cJSON.h> //installata libreria esterna
 
 #define PORT 8080
@@ -80,7 +81,7 @@ void aggiorna_numero_connessioni(int * socket_nuovo){
     if(numero_connessioni>=MAX_GIOCATORI){
         perror("numero max giocatori superato");
         if(socket_nuovo!=NULL){
-            send(socket_nuovo,msg,strlen(msg),0);
+            send(*socket_nuovo,msg,strlen(msg),0);
             close(*socket_nuovo);
             free(socket_nuovo);
         }
@@ -124,7 +125,7 @@ void queue_remove(int id){
 }
 
 
-void handle_client(void *arg){
+void *handle_client(void *arg){
     char buffer[BUFFER_SIZE];
 	char nome[30];
 	int leave_flag = 0;
@@ -135,7 +136,7 @@ void handle_client(void *arg){
     nuovo_giocatore->id = numero_connessioni;
     nuovo_giocatore->in_partita=0;
 
-    int size = read(client_sock, buffer, sizeof(buffer)-1);
+    int size = read(*socket_nuovo, buffer, sizeof(buffer)-1);
     if(size > 0){
         buffer[size] = '\0';
 
@@ -172,11 +173,11 @@ void handle_client(void *arg){
         printf("test_successo");
         leave_flag=1;
           
-        }
-    }
+        }}
     
-        send(nuovo_giocatore->socket,msg2,strlen(msg2),0);
-        close(nuovo_giocatore->socket);
+    
+        send(*(nuovo_giocatore->socket),msg2,strlen(msg2),0);
+        close(*(nuovo_giocatore->socket));
         free(nuovo_giocatore->socket);
         queue_remove(nuovo_giocatore->id);
         free(nuovo_giocatore);
@@ -207,7 +208,7 @@ int main(){
         return 1;
 	}
 
-    if(bind(fd,(struct sockadddr*)&address,sizeof(address))<0){
+    if(bind(fd,(struct sockaddr*)&address,sizeof(address))<0){
         perror("errore bind");
         close(fd);
         return 1;
@@ -221,7 +222,7 @@ int main(){
 
     while(1){
         int * socket_nuovo = (int*)malloc(sizeof(int));
-        if ((socket_nuovo = accept(fd, (struct sockaddr *)&address,(socklen_t)&addrlen)) < 0) {
+        if ((*socket_nuovo = accept(fd, (struct sockaddr *)&address,(socklen_t*)&addrlen)) < 0) {
             perror("errore_accept");
             free(socket_nuovo);
             continue;
@@ -232,7 +233,7 @@ int main(){
         pthread_t thread_id;
         if (pthread_create(&thread_id, NULL, handle_client, socket_nuovo) != 0) {
             perror("errore_pthread_create");
-            send(socket_nuovo,msg1,strlen(msg1),0);
+            send(*socket_nuovo,msg1,strlen(msg1),0);
             close(*socket_nuovo);
             free(socket_nuovo);
         } 
